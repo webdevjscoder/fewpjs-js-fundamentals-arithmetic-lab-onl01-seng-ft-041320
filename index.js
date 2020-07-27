f9a834f58e987bb7cba25b50829c251b75431a9c
webdevjscoder <cobianjuan19@gmail.com>
2020-07-27 12:22:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+const multiply = num1 * num2;
+
+const random = Math.random();
+
+const mod = 16 % 6;
+
+const max = Math.max(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
